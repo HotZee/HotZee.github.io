63ffd3531ef3a995e2030f8e06e2b2a6a45cfc4d
HotZee <53225297+HotZee@users.noreply.github.com>
2019-08-20 14:30:32 +0300

    Add files via upload

--- smart/assets/js/smart.js
+++ smart/assets/js/smart.js
@@ -1,4 +1,5 @@
 // SLIDER
+
 $('#right__arrow').click(function () {
     var currentSlide = $('.slide.active');
     var nextSlide = currentSlide.next();
@@ -22,4 +23,37 @@ $('#left__arrow').click(function () {
     if(prevSlide.length == 0) {
         $('.slide').last().fadeIn(300).addClass('active');
     }
+});
+
+
+// SERVICES
+$(".service__logo").on("click", function (event) {
+    event.preventDefault();
+    $(".service__logo").removeClass("logo__active");
+    $("[data-intro]").removeClass("active");
+    $("[data-image]").removeClass("active");
+    var id = $(this).attr('data-services');
+    var numID = parseInt(id.match(/\d+/));
+    $('[data-intro="#intro'+numID+'"]').addClass('active');
+    $('[data-intro="#intro'+numID+'"]').addClass('animated fadeInUp');
+    $('[data-image="#image'+numID+'"]').addClass('active');
+    $('[data-image="#image'+numID+'"]').addClass('animated fadeInUp');
+    $(this).toggleClass("logo__active");
+});
+
+
+//FADEIN OF SECTION`S DIV
+jQuery(window).scroll(function(){
+    var $sections = $('section');
+    $sections.each(function(i,el){
+        var top  = $('#services').offset().top-450;
+        var bottom = top +$('#services').height();
+        var scroll = $(window).scrollTop();
+        if( scroll > top && scroll < bottom){
+            $('[data-intro="#intro1"]').addClass('animated fadeInUp');
+            $('[data-image="#image1"]').addClass('animated fadeInUp');
+            $('[data-intro="#intro1"]').addClass('active');
+            $('[data-image="#image1"]').addClass('active');
+        }
+    })
 });
