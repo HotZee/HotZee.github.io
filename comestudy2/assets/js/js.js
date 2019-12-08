3bbc682969a6e9903fb69cc65da2de6fa08c144a
HotZee <53225297+HotZee@users.noreply.github.com>
2019-12-09 00:56:11 +0200

    Add files via upload

--- comestudy2/assets/js/js.js
+++ comestudy2/assets/js/js.js
@@ -157,4 +157,17 @@ $(function () {
 
 });
 
+$(".consider_section").appear(function() {
+    $('.number-scroll').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
 
